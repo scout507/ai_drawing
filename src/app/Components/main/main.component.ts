ed6c3c1939c7738538e3dfa04293bf6c7bd5d75b
socke300 <60921761+socke300@users.noreply.github.com>
2022-01-31 14:16:05 +0100

    html changes and rescaler option added

--- src/app/Components/main/main.component.ts
+++ src/app/Components/main/main.component.ts
@@ -19,6 +19,9 @@ export class MainComponent implements AfterViewInit {
   minY = 1000;
   maxX = 0;
   maxY = 0;
+  paintCanvasHeight = 400;
+  paintCanvasWidth = 400;
+  rescalerOn = true;
 
   constructor() {
   }
@@ -30,7 +33,7 @@ export class MainComponent implements AfterViewInit {
     this.context.lineWidth = 2;
     this.context.strokeStyle = "black";
     this.context.fillStyle = "white";
-    this.context.fillRect(0,0,this.paintCanvas.width,this.paintCanvas.height)
+    this.context.fillRect(0, 0, this.paintCanvas.width, this.paintCanvas.height)
     this.addEvents();
     this.minX = this.paintCanvas.width;
     this.minY = this.paintCanvas.height;
@@ -56,7 +59,7 @@ export class MainComponent implements AfterViewInit {
     if (this.isMouseDown) {
       const newX = event.offsetX;
       const newY = event.offsetY;
-      this.trackValues(newX,newY);
+      this.trackValues(newX, newY);
       this.context.beginPath();
       this.context.moveTo(this.x, this.y);
       this.context.lineTo(newX, newY);
@@ -70,28 +73,28 @@ export class MainComponent implements AfterViewInit {
 
     let model: any = await tf.loadLayersModel("http://localhost:4200/assets/model.json");
     console.log(this.minX, this.maxX, this.minY, this.maxY);
-    this.cropImageFromCanvas();
+    if (this.rescalerOn) this.cropImageFromCanvas();
     let inputTensor = tf.browser.fromPixels(this.paintCanvas, 3)// imageResult is an <img/> tag
-      .resizeBilinear([255,255])
+      .resizeBilinear([255, 255])
       .reshape([1, 255, 255, 3])
       .cast('float32');
-    let predictionResult =  model.predict(inputTensor).dataSync();
+    let predictionResult = model.predict(inputTensor).dataSync();
     let recognizedDigit = predictionResult.indexOf(Math.max(...predictionResult));
     let sum = 0;
 
-    predictionResult.forEach((prediction:number) =>{
-      if(prediction>0) sum += prediction;
+    predictionResult.forEach((prediction: number) => {
+      if (prediction > 0) sum += prediction;
     })
 
     this.resultList(predictionResult, sum);
-    document.getElementById("output")!.innerHTML = this.labels[recognizedDigit] + "  " + Math.round(predictionResult[recognizedDigit]*100/sum) + "%";
+    document.getElementById("output")!.innerHTML = this.labels[recognizedDigit] + "  " + Math.round(predictionResult[recognizedDigit] * 100 / sum) + "%";
   }
 
-  trackValues(x: number, y: number){
-    if(x > this.maxX) this.maxX = x;
-    if(x < this.minX) this.minX = x;
-    if(y > this.maxY) this.maxY = y;
-    if(y < this.minY) this.minY = y;
+  trackValues(x: number, y: number) {
+    if (x > this.maxX) this.maxX = x;
+    if (x < this.minX) this.minX = x;
+    if (y > this.maxY) this.maxY = y;
+    if (y < this.minY) this.minY = y;
   }
 
   cropImageFromCanvas() {
@@ -99,13 +102,11 @@ export class MainComponent implements AfterViewInit {
     let w = 1 + this.maxX - this.minX;
     let h = 1 + this.maxY - this.minY;
     const cut = this.context.getImageData(this.minX, this.minY, this.maxX, this.maxY);
-    this.context.lineWidth = 0.2;
 
-    if(w > h) {
+    if (w > h) {
       canvas.width = w;
       canvas.height = w;
-    }
-    else{
+    } else {
       canvas.width = h;
       canvas.height = h;
     }
@@ -113,15 +114,24 @@ export class MainComponent implements AfterViewInit {
   }
 
 
-  resultList(results: any, sum: number){
+  resultList(results: any, sum: number) {
     console.log("Results:")
-      for(let i = 0; i<results.length; i++){
-          if(results[i]>0) console.log(this.labels[i]+ ": "+ results[i] + "  Accuracy: " + (results[i]/sum));
-      }
+    for (let i = 0; i < results.length; i++) {
+      if (results[i] > 0) console.log(this.labels[i] + ": " + results[i] + "  Accuracy: " + (results[i] / sum));
+    }
   }
 
   clear() {
-    this.context.fillRect(0,0,this.paintCanvas.width,this.paintCanvas.height)
+    this.paintCanvas.width = this.paintCanvasWidth;
+    this.paintCanvas.height = this.paintCanvasHeight;
+    this.maxX = 0;
+    this.maxY = 0;
+    this.minX = this.paintCanvasWidth;
+    this.minY = this.paintCanvasHeight;
+    this.context.lineCap = 'round';
+    this.context.lineWidth = 2;
+    this.context.fillStyle = "white";
+    this.context.fillRect(0, 0, this.paintCanvas.width, this.paintCanvas.height)
     document.getElementById("output")!.innerHTML = "";
   }
 
